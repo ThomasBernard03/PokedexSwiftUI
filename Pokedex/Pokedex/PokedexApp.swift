669112997b22a232e0028e36463e3cc0cbe9d8ee
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-07-01 10:38:57 +0200

    project init

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Thomas Bernard on 01/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
