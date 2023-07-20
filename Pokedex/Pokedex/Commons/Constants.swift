4f7ff7da4e7d6e3d27e964f53b3297b393aacaf9
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-07-20 11:26:13 +0200

    add home item view

--- Pokedex/Pokedex/Commons/Constants.swift
+++ Pokedex/Pokedex/Commons/Constants.swift
@@ -6,10 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
 
 
 struct Constants {
     
     static let pokeAPIBaseUrl = "https://pokeapi.co/api/v2/"
     static let pokemonsJSONFilePath = "Pokemons"
+    
+    
+    
+    struct Colors {
+        static let grassColor = Color("GrassColor")
+        static let fireColor = Color("FireColor")
+        static let waterColor = Color("WaterColor")
+        static let electricColor = Color("ElectricColor")
+    }
+    
+    struct Images {
+        static let pokeballBackground = Image("PokeballBackground")
+    }
 }
---
+++ Pokedex/Pokedex/Views/Home/HomeItemView.swift
@@ -0,0 +1,58 @@
+//
+//  HomeItemView.swift
+//  Pokedex
+//
+//  Created by Thomas Bernard on 20/07/2023.
+//
+
+import SwiftUI
+
+struct HomeItemView: View {
+    
+    let title : String
+    let color : Color
+    
+    var body: some View {
+        
+        ZStack(alignment: .leading) {
+            Constants.Images.pokeballBackground
+                .offset(x:-80, y: -40)
+            
+            HStack {
+                Text(title)
+                    .fontWeight(.bold)
+                    .font(.title2)
+                    .foregroundColor(.white)
+                
+                Spacer()
+            }
+            
+            Constants.Images.pokeballBackground
+                .resizable()
+                .frame(width: 150, height: 150)
+                .offset(x:270, y: -10)
+            
+
+            
+        }
+        .padding(.horizontal, 30)
+        .background(color)
+        .frame(width: .infinity, height: 80)
+        .cornerRadius(20)
+    }
+}
+
+struct HomeItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(spacing: 20) {
+            HomeItemView(title: "Pokemons", color: Constants.Colors.grassColor)
+            
+            HomeItemView(title: "Moves", color: Constants.Colors.fireColor)
+            
+            HomeItemView(title: "Abilities", color: Constants.Colors.waterColor)
+            
+            HomeItemView(title: "Items", color: Constants.Colors.electricColor)
+        }
+        .padding(20)
+    }
+}
